2d47018cc3b161a03069b64f5acd319c5f1d8d5d
YiverStiven <yiverstiven@gmail.com>
2023-10-16 18:42:09 -0500

    Primer commit con java spring

---
+++ src/main/java/com/jinbemarket/JinbeMarketApplication.java
@@ -0,0 +1,13 @@
+package com.jinbemarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinbeMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JinbeMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jinbemarket/JinbeMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinbemarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JinbeMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
